4736c581998ffce99f2999ecd223457fbd257d5c
Alexander Clark <sasha.jackal@gmail.com>
2013-02-12 07:32:19 -0700

    Added rails and refactored html to public folder.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spangle::Application.config.secret_token = '7cceda26f4fa0d3a59a2db0fef3f34207db3748de5f70285a5a5b44ae219e82d5ee5bd168daab29c311c3d78b6f2bc83de83357cbd23661d8d08e5d3084b844a'
